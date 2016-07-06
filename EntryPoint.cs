bf2516c8491f397685a9bb3a7ddea3572a8e144a
Liam Crabbe <liam.crabbe.3@gmail.com>
2016-07-05 17:32:39 -0700

    InitialCommit
    Spooky scary skeleton

---
+++ EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+
+namespace SupervisingDuties
+{
+    public class EntryPoint : Plugin
+    {
+        public override void Initialize()
+        {
+
+            // Subscribe to OnOnDutyStateChanged event
+            Functions.OnOnDutyStateChanged += OnDutyStateChangedHandler;
+
+            // Log loading of our plugin
+            Game.LogTrivial("SupervisingDuties v0.0.1, by drsn0w, loaded");
+        }
+
+        private static void OnDutyStateChangedHandler(bool OnDuty)
+        {
+            if (OnDuty)
+            {
+                Game.LogTrivial("Have fun");
+            }
+        }
+
+        public override void Finally()
+        {
+            Game.LogTrivial("SupervisingDuties unloaded");
+        }
+    }
+}
